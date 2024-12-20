aa8654de2cc7b5c6174ea3ab9eae4d99249eca4b
quan <quanpham4908@gmail.com>
2024-12-20 22:18:20 +0700

    video46

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import vn.hoidanit.laptopshop.service.UserService;
 
-@RestController
+@Controller
 public class UserController {
     private UserService userService;
 
@@ -17,8 +17,24 @@ public class UserController {
         this.userService = userService;
     }
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String getHomePage() {
-        return this.userService.handleHello();
+        String test = this.userService.handleHello();
+        return "eric.html";
     }
 }
+// @RestController
+// public class UserController {
+
+// // DI : dependency injection
+// private UserService userService;
+
+// public UserController(UserService userService) {
+// this.userService = userService;
+// }
+
+// @GetMapping("/")
+// public String getHomePage() {
+// return this.userService.handleHello();
+// }
+// }
