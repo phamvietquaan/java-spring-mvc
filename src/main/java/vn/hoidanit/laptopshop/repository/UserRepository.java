97165432f50d638384a9530fe8db5bb0cfa134c6
quan <quanpham4908@gmail.com>
2024-12-23 15:44:09 +0700

    video60

---
+++ src/main/java/vn/hoidanit/laptopshop/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package vn.hoidanit.laptopshop.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import vn.hoidanit.laptopshop.domain.User;
+
+//crud: create, read, update, delete
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User save(User hoidanit);
+}
