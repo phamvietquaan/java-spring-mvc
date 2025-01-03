afba1541db76c2d37c558e525db9816a0a7ed3de
phamvietquaan <quanpham4908@gmail.com>
2025-01-03 20:57:47 +0700

    update/delete_product

--- src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
+++ src/main/java/vn/hoidanit/laptopshop/repository/ProductRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import vn.hoidanit.laptopshop.domain.Product;
+import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
